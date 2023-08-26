ecd442e6cb8e9db76dd22a6dbcb767fa0e4eebb8
Jucaerb <camilo.erazo98@hotmail.com>
2023-08-26 12:47:21 -0500

    PI2-14 Confirmación enlace

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 Auth::routes();
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('home'));
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
