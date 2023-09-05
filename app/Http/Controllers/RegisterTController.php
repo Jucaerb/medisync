b7c46ec724c34e3e48f4c405925193bd69f88481
Felipe-CR <felicampos3942@gmail.com>
2023-09-04 22:24:21 -0500

    Creacion del register controller

--- app/Http/Controllers/RegisterTController.php
+++ app/Http/Controllers/RegisterTController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class RegisterTController extends Controller
 {
-    //
+    protected function create(){
+        return view('admin.register');
+    }
+
+    protected function  save(){
+
+    }
 }
