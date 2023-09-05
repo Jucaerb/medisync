be72a1fbeaed8caf4cc1c0f9cb83d5a8d1beb988
Felipe-CR <felicampos3942@gmail.com>
2023-09-04 23:06:45 -0500

    Function create,save,deleted

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,11 +7,46 @@ use Illuminate\Support\Facades\DB;
 
 class UsersController extends Controller
 {
+
+    protected function index() {
+        $users = DB::table('users')
+            ->orderBy('id')
+            ->get();
+
+        return view('admin.users', [
+            'users' => $users
+        ]);
+    }
+
     protected function user() {
         $users = DB::table('users')->get();
 
         return view('admin.users',[
             'users' => $users
         ]);
+    }
+
+    protected function create(){
+        return view('admin.register');
+    }
+
+    protected function  save(Resquest $request){
+        $user = DB::table('users')->insert(array(
+            'username' => $request->input('username'),
+            'password' => $request->input('password'),
+            'full_name' => $request->input('full_name'),
+            'email' => $request->input('email'),
+            'type_identification' => 'CC',
+            'identification_number' => $request->input('identification_number'),
+            'status' => "ACTIVE"
+        ));
+
+        return redirect()->action('UsersController@index')->with('status', 'Usuario creado correctamente');
+    }
+
+    protected function deleted($id){
+        $user = DB::table('users')->where('id', $id)->delete();
+
+        return redirect()->action('UsersController@index')->with('status', 'Usuario borrada correctamente');
     }
 }
