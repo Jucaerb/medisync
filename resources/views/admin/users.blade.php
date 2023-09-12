70b2844e84d812f08f49c027575e86bb5fb98ad1
Felipe-CR <felicampos3942@gmail.com>
2023-09-12 01:31:48 -0500

    Listado de usuarios

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -2,47 +2,46 @@
 
 @section('content')
     <div class="d-flex  p-5">
-    <div class="container">
-        <h5 class="card-title text-aling-left text-body-title  ">Mira tus empleados activos!</h5>
-        <div class="card mb-3">
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-md">
-                        <thead>
-                        <tr>
-                            <th scope="col">Usuario</th>
-                            <th scope="col">Nombre completo</th>
-                            <th scope="col">Rol</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Identificación</th>
-                            <th scope="col">Estado</th>
+        <div class="container">
+            <h5 class="card-title text-aling-left text-body-title  ">Mira tus empleados activos!</h5>
+            <div class="card mb-3">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-md">
+                            <thead>
+                            <tr>
+                                <th scope="col">Usuario</th>
+                                <th scope="col">Nombre completo</th>
+                                <th scope="col">Rol</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Identificación</th>
+                                <th scope="col">Estado</th>
 
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>Roxana.reina</td>
-                            <td>Roxana Reina</td>
-                            <td>Enfermera</td>
-                            <td>roxana@gmail.com</td>
-                            <td>11111</td>
-                            <td>Activo</td>
-                            <td>
-                                <i class="bi bi-trash"></i>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($users as $users)
+                                <tr>
+                                    <td>{{$users->username}}</td>
+                                    <td>{{$users->full_name}}</td>
+                                    <td>{{$users->role}}</td>
+                                    <td>{{$users->email}}</td>
+                                    <td>{{$users->identification_number}}</td>
+                                    <td>{{$users->status}}</td>
+                                    <td>
+                                        <i class="bi bi-trash"></i>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
                 </div>
 
             </div>
-
+            <a href="{{route('admin.home')}}" class="button-regresar">Regresar</a>
         </div>
-        <a href="{{route('admin.home')}}" class="button-regresar">Regresar</a>
     </div>
-    </div>
-
-
-
 
 @endsection
