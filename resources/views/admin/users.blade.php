f37b635ce0d9284d9842ca4187a80e9afa53bbdf
marisabella2 <maria_isa.galvis@uao.edu.co>
2023-09-15 00:27:30 -0500

    correción tipo letra, creación de boton añadir empleado

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -2,21 +2,26 @@
 
 @section('content')
 
-    <div class="d-flex  p-4">
+    <div class="d-flex p-4">
         <div class="container">
-            <h5 class="card-title text-aling-left text-body-title pb-3 ">Mira tus empleados activos!</h5>
+            <p class="card-title text-aling-left text-body-title pb-3">Mira tus empleados activos!</p>
             <div class="card mb-3">
+                <div class="card-header">
+                    <div class="d-flex justify-content-between align-items-center text-center">
+                        <a href="{{ route('admin.registeruser') }}" class="button-add a">+ Añadir empleado</a>
+                    </div>
+                </div>
                 <div class="card-body">
                     <div class="table-responsive">
                         <table class="table table-md">
                             <thead>
                             <tr>
-                                <th scope="col">Usuario</th>
-                                <th scope="col">Nombre completo</th>
-                                <th scope="col">Rol</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Identificación</th>
-                                <th scope="col">Estado</th>
+                                <th scope="col" class="text-body">Usuario</th>
+                                <th scope="col" class="text-body">Nombre completo</th>
+                                <th scope="col" class="text-body">Rol</th>
+                                <th scope="col" class="text-body">Email</th>
+                                <th scope="col" class="text-body">Identificación</th>
+                                <th scope="col" class="text-body">Estado</th>
                                 <th scope="col" style="max-width: 120px;"></th>
                                 <th scope="col" style="max-width: 120px;"></th>
                             </tr>
@@ -26,11 +31,11 @@
                                 @include('admin.inactiveModal')
                                 @include('admin.activateModal')
                                 <tr>
-                                    <td>{{$user->username}}</td>
-                                    <td>{{$user->full_name}}</td>
-                                    <td>{{$user->role}}</td>
-                                    <td>{{$user->email}}</td>
-                                    <td>{{$user->identification_number}}</td>
+                                    <td class="text-body-table">{{$user->username}}</td>
+                                    <td class="text-body-table">{{$user->full_name}}</td>
+                                    <td class="text-body-table">{{$user->role}}</td>
+                                    <td class="text-body-table">{{$user->email}}</td>
+                                    <td class="text-body-table">{{$user->identification_number}}</td>
                                     <td>
                                         @if($user->status == 'ACTIVE')
                                             <span class="badge rounded-pill bg-success">
