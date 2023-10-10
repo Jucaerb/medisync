e4c79a627e8e1b06b079977ebb505e2646385b27
Felipe-CR <felicampos3942@gmail.com>
2023-10-09 22:22:50 -0500

    Ajuste de variable

--- resources/views/doctor/editpatient.blade.php
+++ resources/views/doctor/editpatient.blade.php
@@ -11,39 +11,39 @@
                     <span class="input-group-text" id="inputGroup-sizing-default">Nombre completo</span>
                     <input id="name" name="name" type="text" class="form-control"
                            aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" maxlength="255"
-                           value="{{$patient->name}}">
+                           value="{{$patients->name}}">
                 </div>
-                <input type="hidden" id="patient_id" name="patient_id" value="{{$patient->id}}">
+                <input type="hidden" id="patient_id" name="patient_id" value="{{$patients->id}}">
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="inputGroup-sizing-default">Cédula</span>
                     <input id="identification" name="identification" type="text" class="form-control"
                            aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" maxlength="255"
-                           value="{{$patient->identification}}">
+                           value="{{$patients->identification}}">
                 </div>
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="inputGroup-sizing-default">Sexo</span>
                     <select id="sex" name="sex" class="form-select" aria-label="Default select example">
                         <option selected>Selecciona una opción</option>
-                        <option value="MEN" {{$patient->sex == 'MEN' ? 'selected':''}}>Hombre</option>
-                        <option value="FEMALE" {{$patient->sex == 'FEMALE' ? 'selected':''}}>Mujer</option>
+                        <option value="MEN" {{$patients->sex == 'MEN' ? 'selected':''}}>Hombre</option>
+                        <option value="FEMALE" {{$patients->sex == 'FEMALE' ? 'selected':''}}>Mujer</option>
                     </select>
                 </div>
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="inputGroup-sizing-default">Fecha de nacimiento</span>
                     <input id="birth_date" name="birth_date" type="text" class="form-control"
                            aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" maxlength="255"
-                           value="{{$patient->birth_date}}">
+                           value="{{$patients->birth_date}}">
                 </div>
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="inputGroup-sizing-default">Fecha de ingreso</span>
                     <input id="in_date" name="in_date" type="text" class="form-control"
                            aria-label="Sizing example input"
-                           aria-describedby="inputGroup-sizing-default" maxlength="255" value="{{$patient->in_date}}">
+                           aria-describedby="inputGroup-sizing-default" maxlength="255" value="{{$patients->in_date}}">
                 </div>
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="inputGroup-sizing-default">Habitación</span>
                     <input id="room" name="room" type="text" class="form-control" aria-label="Sizing example input"
-                           aria-describedby="inputGroup-sizing-default" maxlength="255" value="{{$patient->room}}">
+                           aria-describedby="inputGroup-sizing-default" maxlength="255" value="{{$patients->room}}">
                 </div>
                 <div class="d-flex justify-content-start">
                     <div class="pr-5">
