9dd7e818414949d68becf468a6cc862b5a7613b4
Felipe-CR <felicampos3942@gmail.com>
2023-10-11 16:22:29 -0500

    Funcion en la extraccion de datos de las tablas

--- resources/views/doctor/dashboardpatient.blade.php
+++ resources/views/doctor/dashboardpatient.blade.php
@@ -8,14 +8,14 @@
             <div class="card mb-3 border-0 ">
                 {{-- Filtros --}}
                 <div class="card-header border-0">
-                    <form action="{{ route('activities') }}" method="get">
+                    <form action="{{ route('dashboardpatient') }}" method="get">
                         <div class="d-flex flex-row align-items-start">
                             <input type="text" class="form-control busqueda" name="texto" value="{{ $texto }}">
                             <input type="submit" class="btn btn-primary mx-2 button-buscar" value="Buscar">
+                            <a href="{{ route('createactivity') }}" class="button-card a">+ Añadir actividad</a>
                         </div>
                     </form>
                 </div>
-
                 <!-- Inicio de las tarjetas de pacientes -->
                 <div class="row row-cols-1 row-cols-md-4 g-4">
                     @foreach($patients as $patient)
@@ -31,27 +31,22 @@
                                     </div>
                                     <i class="bi bi-person-square" style="font-size: 3.4rem;"></i>
                                 </div>
-                                @endforeach
                                 <!-- Cuerpo de la tarjeta -->
                                 <div class="card-body">
                                     <ul>
-
-                                        @foreach($activities as $activity)
-                                            <li class="text-body-table">{{ $activity->name_activity}}</li>
-                                        @endforeach
-
+                                        <li class="text-body-table">{{$patient->name_activity}}</li>
                                     </ul>
                                 </div>
                                 <!-- Pie de la tarjeta -->
                                 <div class="card-footer border-0 d-flex justify-content-between align-items-center"
                                      style="padding-left: 10px; padding-right: 10px;">
                                     <a class="button-card a">Ver todas</a>
-                                    <a href="{{ route('createactivity') }}" class="button-card a">+ Añadir actividad</a>
                                 </div>
                             </div>
                         </div>
-
+                    @endforeach
                 </div>
+                {{--                {{$dashboardPatient->links('vendor.pagination.simple-bootstrap-5')}}--}}
                 <!-- Fin de las tarjetas de pacientes -->
             </div>
         </div>
