7480038887f8eb0bd12378c817cc95ffd21ed69f
Felipe-CR <felicampos3942@gmail.com>
2023-10-10 23:03:17 -0500

    Correcion de las rutas del modal

--- resources/views/doctor/deleteActivityModal.blade.php
+++ resources/views/doctor/deleteActivityModal.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="deleteActivityModal" tabindex="-1" aria-labelledby="deleteActivityModalLabel" aria-hidden="true">
+<div class="modal fade" id="deleteActivityModal{{$activity->id}}" tabindex="-1" aria-labelledby="deleteActivityModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header" style="border-bottom: none;" >
@@ -9,8 +9,9 @@
                 <p class="text-body-medium">Eliminar actividad</p>
                 <p class="only-text-regular">¿Está seguro de que desea eliminar esta actividad?</p>
             </div>
-            <form action="{{route('home')}}" method="GET">
+            <form action="{{route('deleteactivity', ['id' => $activity->id])}}" method="POST">
                 @csrf
+                @method('DELETE')
                 <div class="modal-footer" style="border-top: none;">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
                     <button type="submit" class="button-modal">Confirmar</button>
