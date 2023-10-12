a8ed9eb9ddbc185f73be5eba55c6dd69fb3d8d21
Felipe-CR <felicampos3942@gmail.com>
2023-10-12 02:05:57 -0500

    Se agrega columna a las migraciones

--- database/migrations/2023_09_27_015431_create_activities_table.php
+++ database/migrations/2023_09_27_015431_create_activities_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('activities', function (Blueprint $table) {
             $table->id();
-            $table->integer('patient_id');
+            $table->string('id_patient');
             $table->string('patient');
             $table->string('name_activity');
             $table->string('min_permissions');
