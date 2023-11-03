43439d5d04b710a705df35c47c4ad7cd23ab404f
Felipe-CR <felicampos3942@gmail.com>
2023-11-03 03:28:36 -0500

    Cambios en schedule

--- resources/views/doctor/editActivity.blade.php
+++ resources/views/doctor/editActivity.blade.php
@@ -19,11 +19,14 @@
                     <select id="min_permissions" name="min_permissions" class="form-select"
                             aria-label="Default select example" required>
                         <option>Selecciona una opción</option>
-                        <option value="DOCTOR" {{$activity->min_permissions == 'DOCTOR' ? 'selected':''}}>Doctor</option>
-                        <option value="BOSS_NURSE" {{$activity->min_permissions == 'BOSS_NURSE' ? 'selected':''}}>Jefe de
+                        <option value="DOCTOR" {{$activity->min_permissions == 'DOCTOR' ? 'selected':''}}>Doctor
+                        </option>
+                        <option value="BOSS_NURSE" {{$activity->min_permissions == 'BOSS_NURSE' ? 'selected':''}}>Jefe
+                            de
                             enfermeria
                         </option>
-                        <option value="NURSE" {{$activity->min_permissions == 'NURSE' ? 'selected':''}}>Enfermero</option>
+                        <option value="NURSE" {{$activity->min_permissions == 'NURSE' ? 'selected':''}}>Enfermero
+                        </option>
                     </select>
                 </div>
                 <div class="input-group mb-3">
@@ -31,14 +34,17 @@
                     <select id="temporality" name="temporality" class="form-select" aria-label="Default select example"
                             required>
                         <option>Selecciona una opción</option>
-                        <option value="diary" {{$activity->temporality == 'diary' ? 'select':''}}>Diario</option>
-                        <option value="every 4 hours" {{$activity->temporality == 'every 4 hours' ? 'selected':''}}>Cada 4
+                        <option value="diary" {{$activity->temporality == 'diary' ? 'selected':''}}>Diario</option>
+                        <option value="every 4 hours" {{$activity->temporality == 'every 4 hours' ? 'selected':''}}>Cada
+                            4
                             horas
                         </option>
-                        <option value="every 8 hours" {{$activity->temporality == 'every 8 hours' ? 'selected':''}}>Cada 8
+                        <option value="every 8 hours" {{$activity->temporality == 'every 8 hours' ? 'selected':''}}>Cada
+                            8
                             horas
                         </option>
-                        <option value="every 12 hours" {{$activity->temporality == 'every 12 hours' ? 'selected':''}}>Cada
+                        <option value="every 12 hours" {{$activity->temporality == 'every 12 hours' ? 'selected':''}}>
+                            Cada
                             12 horas
                         </option>
                         <option value="weekly" {{$activity->temporality == 'weekly' ? 'selected':''}}>Semanal</option>
@@ -65,42 +71,18 @@
                     <select id="schedule" name="schedule" class="form-select" aria-label="Default select example"
                             required>
                         <option>Selecciona una opción</option>
-                        <option value="00:00 - 02:00" {{$activity->schedule == '00:00 - 02:00' ? 'selected':''}}>00:00 -
-                            02:00
-                        </option>
-                        <option value="02:00 - 04:00" {{$activity->schedule == '02:00 - 04:00' ? 'selected':''}}>02:00 -
-                            04:00
-                        </option>
-                        <option value="04:00 - 06:00" {{$activity->schedule == '04:00 - 06:00' ? 'selected':''}}>04:00 -
-                            06:00
-                        </option>
-                        <option value="06:00 - 08:00" {{$activity->schedule == '06:00 - 08:00' ? 'selected':''}}>06:00 -
-                            08:00
-                        </option>
-                        <option value="08:00 - 10:00" {{$activity->schedule == '08:00 - 10:00' ? 'selected':''}}>08:00 -
-                            10:00
-                        </option>
-                        <option value="10:00 - 12:00" {{$activity->schedule == '10:00 - 12:00' ? 'selected':''}}>10:00 -
-                            12:00
-                        </option>
-                        <option value="12:00 - 14:00" {{$activity->schedule == '12:00 - 14:00' ? 'selected':''}}>12:00 -
-                            14:00
-                        </option>
-                        <option value="14:00 - 16:00" {{$activity->schedule == '14:00 - 16:00' ? 'selected':''}}>14:00 -
-                            16:00
-                        </option>
-                        <option value="16:00 - 18:00" {{$activity->schedule == '16:00 - 18:00' ? 'selected':''}}>16:00 -
-                            18:00
-                        </option>
-                        <option value="18:00 - 20:00" {{$activity->schedule == '18:00 - 20:00' ? 'selected':''}}>18:00 -
-                            20:00
-                        </option>
-                        <option value="20:00 - 22:00" {{$activity->schedule == '20:00 - 22:00' ? 'selected':''}}>20:00 -
-                            22:00
-                        </option>
-                        <option value="22:00 - 00:00" {{$activity->schedule == '22:00 - 00:00' ? 'selected':''}}>22:00 -
-                            00:00
-                        </option>
+                        <option value="00:00" {{$activity->schedule == '00:00' ? 'selected':''}}>00:00</option>
+                        <option value="02:00" {{$activity->schedule == '02:00' ? 'selected':''}}>02:00</option>
+                        <option value="04:00" {{$activity->schedule == '04:00' ? 'selected':''}}>04:00</option>
+                        <option value="06:00" {{$activity->schedule == '06:00' ? 'selected':''}}>06:00</option>
+                        <option value="08:00" {{$activity->schedule == '08:00' ? 'selected':''}}>08:00</option>
+                        <option value="10:00" {{$activity->schedule == '10:00' ? 'selected':''}}>10:00</option>
+                        <option value="12:00" {{$activity->schedule == '12:00' ? 'selected':''}}>12:00</option>
+                        <option value="14:00" {{$activity->schedule == '14:00' ? 'selected':''}}>14:00</option>
+                        <option value="16:00" {{$activity->schedule == '16:00' ? 'selected':''}}>16:00</option>
+                        <option value="18:00" {{$activity->schedule == '18:00' ? 'selected':''}}>18:00</option>
+                        <option value="20:00" {{$activity->schedule == '20:00' ? 'selected':''}}>20:00</option>
+                        <option value="22:00" {{$activity->schedule == '22:00' ? 'selected':''}}>22:00</option>
                     </select>
                 </div>
                 <div class="input-group mb-3">
@@ -146,7 +128,7 @@
                         <button type="submit" class="button-usuario" style="margin-right: 5px;">Guardar</button>
                     </div>
                     <div>
-                        <a href="{{route('activities')}}" class="button-regresar a">Cancelar</a>
+                        <a href="{{route('dashboardpatient')}}" class="button-regresar a">Cancelar</a>
                     </div>
                 </div>
             </form>
