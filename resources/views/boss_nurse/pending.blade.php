6cc3f17c04621128c859f230470a1240b09aece9
marisabella2 <maria_isa.galvis@uao.edu.co>
2023-11-04 00:15:57 -0500

    visualización de actividades pendientes

--- resources/views/boss_nurse/pending.blade.php
+++ resources/views/boss_nurse/pending.blade.php
@@ -20,49 +20,66 @@
                 <div class="row row-cols-1 row-cols-md-4 g-4">
                     @foreach($patients as $patient)
                         <div class="col">
-                            <div class="card" style="height: auto;">
+                            <div class="card" style="height: 100%; display: flex; flex-direction: column;">
                                 <!-- Encabezado de la tarjeta -->
                                 <div class="card-header border-0"
                                      style="display: flex; justify-content: space-between;">
-                                    <div>
+                                    <div style="width: 100%;">
                                         <p class="text-body-table"><strong>{{ $patient->name }}</strong></p>
                                         <p class="text-body-table"
                                            id="num_identification">{{ $patient->identification }}</p>
+                                        <table>
+                                            <thead>
+                                            <tr>
+                                                <th scope="col" class="text-body padding-card">Actividad</th>
+                                                <th scope="col" class="text-body">Hora</th>
+                                            </tr>
+                                            </thead>
+                                        </table>
                                     </div>
-                                    <i class="bi bi-person-square" style="font-size: 3.4rem;"></i>
                                 </div>
                                 <!-- Cuerpo de la tarjeta -->
-                                <div class="card-content" style="max-height: 400px; overflow: auto;">
+                                <div class="card-content" style="overflow: auto;">
                                     <div class="card-body">
-                                        <div class="table-responsive">
+                                        <div class="table-responsive" style="max-height: 175px;">
                                             <table class="table table-md">
-                                                <thead>
-                                                <tr>
-                                                    <th scope="col" class="text-body">Actividad</th>
-                                                    <th scope="col" class="text-body">Hora</th>
-                                                    <th scope="col" style="max-width: 120px;"></th>
-                                                </tr>
-                                                </thead>
                                                 <tbody>
+                                                @php $shownAttention = 0; @endphp
                                                 @foreach($attention as $key => $value)
-                                                    @if($key == $patient->id)
+                                                    @if($key == $patient->id && count($value) > 0)
                                                         @foreach($value as $jsonData)
-{{--                                                            @if()--}}
-
-{{--                                                            @endif--}}
+                                                            @if($shownAttention <3)
+                                                            <div class="card-item">
+                                                                <div class="activity-info">
+                                                                    <p class="text-body-table">{{ $jsonData->activity_name }}</p>
+                                                                    <p class="text-body-table">{{ $jsonData->date_for }}</p>
+                                                                </div>
+                                                            </div>
+                                                                @php $shownAttention++; @endphp
+                                                            @endif
                                                         @endforeach
+                                                        @if (count($value) > $shownAttention)
+                                                            <div class="card-body">
+                                                                <ul>
+                                                                    <li class="text-body-table">
+                                                                        <span>...</span>
+                                                                    </li>
+                                                                </ul>
+                                                            </div>
+                                                        @endif
                                                     @endif
                                                 @endforeach
+                                                @if ($shownAttention == 0)
+                                                    <p class="text-body-table">No hay actividades.</p>
+                                                @endif
                                                 </tbody>
                                             </table>
                                         </div>
                                     </div>
                                 </div>
                                 <!-- Pie de la tarjeta -->
-                                <div class="card-footer border-0 d-flex justify-content-between align-items-center"
-                                     style="padding-left: 10px; padding-right: 10px;">
-                                    <a class="button-card a" href="{{route('activities', ['filter' => $patient->id])}}">Ver
-                                        todas</a>
+                                <div class="card-footer border-0 d-flex justify-content-between" style="margin-top: auto;">
+                                    <a class="button-card a" href="{{route('activities', ['filter' => $patient->id])}}">Ver todas</a>
                                 </div>
                             </div>
                         </div>
