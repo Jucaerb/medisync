5456d4186292492bbaa7f4ac5389f31dbd3ccf77
marisabella2 <maria_isa.galvis@uao.edu.co>
2023-11-07 00:41:33 -0500

    creación modal ver attention

---
+++ resources/views/boss_nurse/attentionModal.blade.php
@@ -0,0 +1,60 @@
+<div class="modal fade" id="attentionModal{{''}}" tabindex="-1" aria-labelledby="attentionModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header" style="border-bottom: none;">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+                <p class="text-body-medium"><strong>Actividad xxx </strong></p>
+            </div>
+            <div class="d-flex justify-content-center align-items-center p-5">
+                <ul class="list-unstyled text-center">
+                    <li class="mb-3">
+                        <p class="only-text-regular">Medicamento:></p>
+                    </li>
+                    <li class="mb-3">
+                        <p class="only-text-regular">Via: </p>
+                    </li>
+                    <li class="mb-3">
+                        <p class="only-text-regular">Dosis: </p>
+                    </li>
+                    <li>
+                        <p class="only-text-regular">Hora: </p>
+                    </li>
+                    <li>
+                        <p class="only-text-regular">Fecha: </p>
+                    </li>
+                    <li>
+                        <p class="only-text-regular">Paciente: </p>
+                    </li>
+                    <li>
+                        <p class="only-text-regular">Habitación: </p>
+                    </li>
+                    <li>
+                        <p class="only-text-regular">Médico: </p>
+                    </li>
+                    <li>
+                        <p class="only-text-regular">El usuario que realice esta operación debe ser minimo un </p>
+                    </li>
+                    <li>
+                        <p class="only-text-regular">Observaciones: </p>
+                    </li>
+                </ul>
+            </div>
+
+
+
+            {{--            <form action="{{route('attentionModal', [''])}}" method="POST">--}}
+            {{--                @csrf--}}
+            {{--                @method('PUT')--}}
+            {{--                <div class="modal-footer" style="border-top: none;">--}}
+            {{--                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>--}}
+            {{--                    <button type="submit" class="button-modal">Confirmar</button>--}}
+            {{--                </div>--}}
+            {{--            </form>--}}
+        </div>
+    </div>
+</div>
+
+
