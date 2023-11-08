cbedc0f164d69ebdae414dfec9b6250c5ee754c4
Felipe-CR <felicampos3942@gmail.com>
2023-11-08 00:45:30 -0500

    Creacion de nuevas function

--- app/Http/Controllers/BossNurseController.php
+++ app/Http/Controllers/BossNurseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Activities;
 use App\Models\Attention;
+use App\Models\Patient;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -44,7 +45,40 @@ class BossNurseController extends Controller
             ->orderBy('name','asc')
             ->paginate(8);
 
-        return view('boss_nurse.pending', compact('patients','attention', 'texto'));
+        return view('boss_nurse.pending', compact('patients', 'attention', 'texto'));
+    }
+
+    protected function attentionModal($id, $activity_name) {
+
+        $patients = Patient::find($id);
+        $attention = Attention::find($activity_name);
+        $activities = Activities::where('id_patient', $id)->get();
+
+        return view('boss_nurse.registerAttention', compact('activities', 'attention', 'patients'));
+    }
+
+    Protected function pendingList(Request $request){
+
+        $texto=trim($request->get('texto'));
+
+        $attention = Attention::indexAttention(intval($request->get('filter')), $texto);
+
+        $activities = DB::table('activities')
+            ->select('id','patient','name_activity','min_permissions','temporality',
+                'schedule','medicine_id','dose','via','observations','create_date','suspension_date')
+            ->where('name_activity', 'LIKE', '%'.$texto.'%' )
+            ->orWhere('min_permissions', 'LIKE', '%'.$texto.'%')
+            ->orderBy('name_activity')
+            ->paginate(6);
+
+        $patients =DB::table('patients')
+            ->select('id', 'identification','name','sex', 'birth_date','in_date','room','status')
+            ->where('name','LIKE', '%'.$texto.'%')
+            ->orWhere('identification', 'LIKE', '%'.$texto.'%')
+            ->orderBy('name','asc')
+            ->paginate(8);
+
+        return view('boss_nurse.pendingList', compact('activities', 'patients','attention', 'texto'));
     }
 
     public function show($user_id){
--- app/Models/Attention.php
+++ app/Models/Attention.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use function Laravel\Prompts\text;
 
 class Attention extends Model
 {
@@ -21,4 +22,21 @@ class Attention extends Model
         'permissions',
         'status'
     ];
+
+    public static function attetionPatient($request) {
+        return Attention::updatedOrCreate(['id' => $request],[
+            "status" => 'INACTIVE',
+        ]);
+    }
+
+    public static function indexAttention($user_id, $texto) {
+        return self::
+            when((!empty($texto)), function ($cons) use ($texto){
+                return $cons->where('activity_name', 'LIKE', '%'.$texto.'%')
+                    ->orWhere('permissions', 'LIKE', '%'.$texto.'%');
+        })
+            ->when(($user_id != null), function ($query) use ($user_id){
+                return $query->where('user_id', $user_id);
+            })->orderBy('activity_name', 'asc')->paginate(8);
+    }
 }
