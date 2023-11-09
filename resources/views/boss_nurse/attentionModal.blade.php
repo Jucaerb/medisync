29bf94d47b9dee4341bf2e7fd81f498a760cd722
marisabella2 <maria_isa.galvis@uao.edu.co>
2023-11-09 01:06:27 -0500

    modales de atenciones

--- resources/views/boss_nurse/attentionModal.blade.php
+++ resources/views/boss_nurse/attentionModal.blade.php
@@ -1,37 +1,52 @@
-<div class="modal fade" id="attentionModal{{$jsonData->id}}" tabindex="-1" aria-labelledby="attentionModalLabel"
-     aria-hidden="true">
-    <div class="modal-dialog">
+<div class="modal fade" id="attentionModal{{$jsonData->id}}" aria-hidden="true" aria-labelledby="attentionModalToggleLabel" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
-            <div class="modal-header" style="border-bottom: none;">
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+            <div class="modal-header">
+                <div class="modal-body text-center d-flex justify-content-between text-center">
+                    <p class="text-body-medium mb-0"><strong>Actividad {{$jsonData->activity_name}}</strong></p>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
             </div>
-            <div class="modal-body text-center">
-                <p class="text-body-medium"><strong>Actividad {{$jsonData->activity_name}} </strong></p>
-            </div>
-            <div class="d-flex justify-content-center align-items-center p-5">
-                <ul class="list-unstyled text-center">
+            <div class="modal-body">
+                <div class="d-flex justify-content-center align-items-center">
+                    <ul class="list-unstyled text-center">
                         <li class="mb-3">
-                            <p class="only-text-regular">Medicamento: {{ $jsonData->medicine_id }}</p>
+                            <p class="text-body"><strong>Medicamento:</strong> {{ $jsonData->medicine_id }}</p>
                         </li>
                         <li class="mb-3">
-                            <p class="only-text-regular">Via: {{ $jsonData->via }}</p>
+                            <p class="text-body"><strong>Via:</strong> {{ $jsonData->via }}</p>
                         </li>
                         <li class="mb-3">
-                            <p class="only-text-regular">Dosis: {{ $jsonData->dose }}</p>
+                            <p class="text-body"><strong>Dosis:</strong> {{ $jsonData->dose }}</p>
                         </li>
                         <li>
-                            <p class="only-text-regular">Hora: {{ \Carbon\Carbon::createFromFormat('H',$jsonData->hour)->format('H:i') }}</p>
+                            <p class="text-body"><strong>Hora:</strong> {{ \Carbon\Carbon::createFromFormat('H',$jsonData->hour)->format('H:i') }}</p>
                         </li>
-                </ul>
+                        <li>
+                            <p class="text-body"><strong>Fecha:</strong> {{$jsonData->date_for}}</p>
+                        </li>
+                        <li>
+                            <p class="text-body"><strong>Paciente:</strong> {{$jsonData->name}} </p>
+                        </li>
+                        <li>
+                            <p class="text-body"><strong>Habitación:</strong> {{$jsonData->room}}</p>
+                        </li>
+                        <li>
+                            <p class="text-body">El usuario que realice esta operación debe <br>ser mínimo un <strong>{{$jsonData->min_permissions}}</strong> </p>
+                        </li>
+                        <li>
+                            <p class="text-body"><strong>Observaciones:</strong> {{$jsonData->observations}}</p>
+                        </li>
+
+                    </ul>
+                </div>
             </div>
+            <div class="modal-footer">
+                <button class="button-modal" data-bs-target="#registerAttentionModal{{$jsonData->id}}" data-bs-toggle="modal">Atender</button>
+                <button class="btn btn-secondary" data-bs-toggle="modal">Cancelar</button>
+            </div>
+
         </div>
     </div>
-{{--    <form action="{{route('attentionModal', [''])}}" method="POST">--}}
-{{--        @csrf--}}
-{{--        @method('PUT')--}}
-{{--        <div class="modal-footer" style="border-top: none;">--}}
-{{--            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>--}}
-{{--            <button type="submit" class="button-modal">Confirmar</button>--}}
-{{--        </div>--}}
-{{--    </form>--}}
 </div>
---
+++ resources/views/boss_nurse/registerAttentionModal.blade.php
@@ -0,0 +1,42 @@
+<div class="modal fade" id="registerAttentionModal{{$jsonData->id}}" aria-hidden="true"
+     aria-labelledby="registerAttentionModalToggleLabel" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="modal-body text-center d-flex justify-content-between text-center">
+                    <p class="text-body-medium mb-0"><strong>Registra la atención realizada</strong></p>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+            </div>
+            <div class="modal-body">
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Paciente</span>
+                    <input class="form-control" type="text" value="{{$jsonData->name}}"
+                           aria-label="Disabled input example"
+                           disabled readonly>
+                </div>
+
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Nombre actividad</span>
+                    <input class="form-control" type="text" value="{{$jsonData->activity_name}}"
+                           aria-label="Disabled input example" disabled readonly>
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Comentarios</span>
+                    <input id="comments" name="comments" type="text" class="form-control"
+                           aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"
+                           maxlength="400" required>
+                </div>
+            </div>
+
+            {{--            <form action="{{route('activePatient', ['id' => $patient->id])}}" method="POST">--}}
+            {{--                @csrf--}}
+            {{--                @method('PUT')--}}
+                        <div class="modal-footer">
+                            <button class="button-modal" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Guardar</button>
+                            <button class="btn btn-secondary" data-bs-toggle="modal">Cancelar</button>
+                        </div>
+            {{--            </form>--}}
+        </div>
+    </div>
+</div>
